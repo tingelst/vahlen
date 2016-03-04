5c98a4d649205ed2c3f61742b0d459dfc5e76076
Lars Tingelstad <lars.tingelstad@ntnu.no>
2016-03-04 15:38:07 +0100

    Added more operations

--- include/vahlen/vahlen.h
+++ include/vahlen/vahlen.h
@@ -4,22 +4,24 @@
 
 namespace vahlen {
 
-template <typename T>
-using Matrix = Eigen::Matrix<T, 8, 8>;
+template <typename T> using Matrix = Eigen::Matrix<T, 8, 8>;
 
-template <typename T>
-using Matrix44 = Eigen::Matrix<T, 4, 4>;
+template <typename T> using Matrix44 = Eigen::Matrix<T, 4, 4>;
 
-template <typename T>
-using Matrix22 = Eigen::Matrix<T, 2, 2>;
+template <typename T> using Matrix22 = Eigen::Matrix<T, 2, 2>;
 
-template <typename T>
-Matrix<T> Id() {
-  return Matrix<T>::Identity();
+template <typename T> Matrix<T> Mnk() {
+  Matrix<T> m = Matrix<T>::Identity();
+  m(4, 4) = T(-1.0);
+  m(5, 5) = T(-1.0);
+  m(6, 6) = T(-1.0);
+  m(7, 7) = T(-1.0);
+  return m;
 }
 
-template <typename T>
-Matrix<T> E1() {
+template <typename T> Matrix<T> Id() { return Matrix<T>::Identity(); }
+
+template <typename T> Matrix<T> E1() {
   Matrix<T> m = Matrix<T>::Zero();
   m(2, 0) = T(1.0);
   m(3, 1) = T(1.0);
@@ -32,8 +34,7 @@ Matrix<T> E1() {
   return m;
 }
 
-template <typename T>
-Matrix<T> E2() {
+template <typename T> Matrix<T> E2() {
   Matrix<T> m = Matrix<T>::Zero();
   m(3, 0) = T(1.0);
   m(2, 1) = T(-1.0);
@@ -46,8 +47,7 @@ Matrix<T> E2() {
   return m;
 }
 
-template <typename T>
-Matrix<T> E3() {
+template <typename T> Matrix<T> E3() {
   Matrix<T> m = Matrix<T>::Identity();
   m(2, 2) = T(-1.0);
   m(3, 3) = T(-1.0);
@@ -56,28 +56,15 @@ Matrix<T> E3() {
   return m;
 }
 
-template <typename T>
-Matrix<T> E12() {
-  return E1<T>() * E2<T>();
-}
+template <typename T> Matrix<T> E12() { return E1<T>() * E2<T>(); }
 
-template <typename T>
-Matrix<T> E13() {
-  return E1<T>() * E3<T>();
-}
+template <typename T> Matrix<T> E13() { return E1<T>() * E3<T>(); }
 
-template <typename T>
-Matrix<T> E23() {
-  return E2<T>() * E3<T>();
-}
+template <typename T> Matrix<T> E23() { return E2<T>() * E3<T>(); }
 
-template <typename T>
-Matrix<T> E123() {
-  return E1<T>() * E2<T>() * E3<T>();
-}
+template <typename T> Matrix<T> E123() { return E1<T>() * E2<T>() * E3<T>(); }
 
-template <typename T>
-Matrix<T> Ni() {
+template <typename T> Matrix<T> Ni() {
   Matrix<T> m = Matrix<T>::Zero();
   m(0, 4) = T(-2.0);
   m(1, 5) = T(-2.0);
@@ -86,8 +73,7 @@ Matrix<T> Ni() {
   return m;
 }
 
-template <typename T>
-Matrix<T> No() {
+template <typename T> Matrix<T> No() {
   Matrix<T> m = Matrix<T>::Zero();
   m(4, 0) = T(1.0);
   m(5, 1) = T(1.0);
@@ -96,8 +82,29 @@ Matrix<T> No() {
   return m;
 }
 
-template <typename T>
-Matrix44<T> CliffordConjugate(const Matrix44<T> x) {
+template <typename T> Matrix22<T> ComplexConjugate(const Matrix22<T> x) {
+  T a = x(0, 0);
+  T b = x(0, 1);
+  T c = x(1, 0);
+  T d = x(1, 1);
+  Matrix22<T> m;
+  m << a, -b, -c, d;
+  return m;
+}
+template <typename T> Matrix44<T> Reverse(const Matrix44<T> x) {
+  Matrix22<T> a = x.topLeftCorner(2, 2);
+  Matrix22<T> b = x.topRightCorner(2, 2);
+  Matrix22<T> c = x.bottomLeftCorner(2, 2);
+  Matrix22<T> d = x.bottomRightCorner(2, 2);
+  Matrix44<T> m;
+  m.topLeftCorner(2, 2) = ComplexConjugate(a);
+  m.topRightCorner(2, 2) = ComplexConjugate(c);
+  m.bottomLeftCorner(2, 2) = ComplexConjugate(b);
+  m.bottomRightCorner(2, 2) = ComplexConjugate(d);
+  return m;
+}
+
+template <typename T> Matrix44<T> CliffordConjugate(const Matrix44<T> x) {
   Matrix22<T> a = x.topLeftCorner(2, 2);
   Matrix22<T> b = x.topRightCorner(2, 2);
   Matrix22<T> c = x.bottomLeftCorner(2, 2);
@@ -110,8 +117,23 @@ Matrix44<T> CliffordConjugate(const Matrix44<T> x) {
   return m;
 }
 
-template <typename T>
-Matrix<T> Reverse(const Matrix<T>& x) {
+template <typename T> Matrix<T> Inverse(const Matrix<T> &x) {
+  Matrix44<T> a = x.topLeftCorner(4, 4);
+  Matrix44<T> b = x.topRightCorner(4, 4);
+  Matrix44<T> c = x.bottomLeftCorner(4, 4);
+  Matrix44<T> d = x.bottomRightCorner(4, 4);
+
+  Matrix44<T> det_matrix = a * Reverse(d) - b * Reverse(c);
+  T det = det_matrix(0, 0);
+
+  Matrix<T> m;
+  m.topLeftCorner(4, 4) = Reverse(d);
+  m.topRightCorner(4, 4) = -Reverse(b);
+  m.bottomLeftCorner(4, 4) = -Reverse(c);
+  m.bottomRightCorner(4, 4) = Reverse(a);
+  return m / det;
+}
+template <typename T> Matrix<T> Reverse(const Matrix<T> &x) {
   Matrix44<T> a = x.topLeftCorner(4, 4);
   Matrix44<T> b = x.topRightCorner(4, 4);
   Matrix44<T> c = x.bottomLeftCorner(4, 4);
@@ -125,34 +147,53 @@ Matrix<T> Reverse(const Matrix<T>& x) {
   return m;
 }
 
-
 template <typename T>
-Matrix<T> Point(const T* x) {
-  Matrix<T> m = x[0] * E1<T>() + x[1] * E2<T>() + x[2] * E3<T>();
-  return m + T(0.5) * m * m * Ni<T>() + No<T>();
+Matrix<T> InnerProduct(const Matrix<T> &a, const Matrix<T> &b) {
+  return T(0.5) * (a * b - b * a);
 }
 
 template <typename T>
-Matrix<T> Point(const Matrix<T>& x) {
+Matrix<T> OuterProduct(const Matrix<T> &a, const Matrix<T> &b) {
+  return T(0.5) * (a * b + b * a);
+}
+
+template <typename T>
+Matrix<T> ProjectVectorBivector(const Matrix<T> &a, const Matrix<T> &b) {
+  return InnerProduct(a, b) * Inverse(b);
+}
+
+template <typename T>
+Matrix<T> RejectVectorBivector(const Matrix<T> &a, const Matrix<T> &b) {
+  return OuterProduct(a, b) * Inverse(b);
+}
+
+template <typename T> Matrix<T> Point(const Matrix<T> &x) {
   return x + T(0.5) * x * x * Ni<T>() + No<T>();
 }
 
-template <typename T>
-Matrix<T> Rotor(const T* x) {
+template <typename T> Matrix<T> Point(const T x, const T y, const T z) {
+  Matrix<T> m = x * E1<T>() + y * E2<T>() + z * E3<T>();
+  return Point<T>(m);
+}
+
+template <typename T> Matrix<T> Point(const T *x) {
+  Matrix<T> m = x[0] * E1<T>() + x[1] * E2<T>() + x[2] * E3<T>();
+  return Point<T>(m);
+}
+
+template <typename T> Matrix<T> Rotor(const T *x) {
   return x[0] * Id<T>() + x[1] * E12<T>() + x[2] * E13<T>() + x[3] * E23<T>();
 }
 
-template <typename T>
-Matrix<T> Translator(const T* x) {
+template <typename T> Matrix<T> Translator(const T *x) {
   return x[0] * Id<T>() + x[1] * E1<T>() * Ni<T>() + x[2] * E2<T>() * Ni<T>() +
          x[3] * E3<T>() * Ni<T>();
 }
 
-template <typename T>
-Matrix<T> Motor(const T* x) {
+template <typename T> Matrix<T> Motor(const T *x) {
   return x[0] * Id<T>() + x[1] * E12<T>() + x[2] * E13<T>() + x[3] * E23<T>() +
          x[4] * E1<T>() * Ni<T>() + x[5] * E2<T>() * Ni<T>() +
          x[6] * E3<T>() * Ni<T>() + x[7] * E123<T>() * Ni<T>();
 }
 
-}  // namespace vahlen
+} // namespace vahlen
--- src/motor.cpp
+++ src/motor.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <Eigen/Core>
 
 #include <vahlen/vahlen.h>
 
@@ -10,9 +9,9 @@ using Mat = vahlen::Matrix<double>;
 int main() {
   // std::cout << vahlen::id<double>() << std::endl;
 
-  Mat e1 = vahlen::E1<double>();
-  Mat e2 = vahlen::E2<double>();
-  Mat e3 = vahlen::E3<double>();
+  Mat e1 = vahlen::E1<double>() * vahlen::Mnk<double>();
+  Mat e2 = vahlen::E2<double>() * vahlen::Mnk<double>();
+  Mat e3 = vahlen::E3<double>() * vahlen::Mnk<double>();
   Mat ni = vahlen::Ni<double>();
   Mat no = vahlen::No<double>();
 
@@ -28,7 +27,7 @@ int main() {
   std::cout << std::endl;
   std::cout << e2 * e1 << std::endl;
   std::cout << std::endl;
-  Mat e12 = e1 * e2;
+  Mat e12 = 0.5 * (e1 * e2 - e2 * e1);
   std::cout << vahlen::Reverse(e12) << std::endl;
   std::cout << std::endl;
 
@@ -55,6 +54,49 @@ int main() {
   Mat translator = vahlen::Translator<double>(t);
 
   std::cout << translator * no * vahlen::Reverse(translator) << std::endl;
+  std::cout << std::endl;
+
+  Mat motor = translator * rotor;
+  std::cout << motor << std::endl;
+
+  vahlen::Matrix44<double> A =
+      (motor * vahlen::Mnk<double>()).topLeftCorner(4, 4);
+  std::cout << A << std::endl;
+  std::cout << std::endl;
+
+  std::cout << vahlen::InnerProduct<double>(e1, e12) << std::endl;
+  std::cout << std::endl;
+  std::cout << e2 << std::endl;
+  std::cout << std::endl;
+
+  vahlen::Matrix22<double> complex;
+  complex << 1.0, -2.0, 2.0, 1.0;
+  std::cout << complex << std::endl;
+  std::cout << std::endl;
+  std::cout << vahlen::ComplexConjugate(complex) << std::endl;
+  std::cout << std::endl;
+
+  Mat biv = vahlen::E12<double>() + 2. * vahlen::E13<double>() +
+            3. * vahlen::E23<double>();
+  std::cout << vahlen::Inverse(biv) << std::endl;
+  std::cout << std::endl;
+  std::cout << std::endl;
+
+  std::cout << vahlen::E1<double>() << std::endl;
+  std::cout << std::endl;
+  std::cout << vahlen::E2<double>() << std::endl;
+  std::cout << std::endl;
+  std::cout << vahlen::E3<double>() << std::endl;
+  std::cout << std::endl;
+
+
+  double m_arr[8] = {1., 2., 3., 4., 5., 6., 7., 8.};
+  Mat mot = vahlen::Motor<double>(m_arr);
+  vahlen::Matrix44<double> rot = mot.topLeftCorner(4,4);
+  vahlen::Matrix44<double> trans = mot.topRightCorner(4,4);
+  
+  std::cout << trans * vahlen::Reverse(rot) << std::endl;
+  std::cout << std::endl;
 
   return 0;
 }
